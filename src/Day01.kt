9e5b1058302eb2020270d7f595cea1d5a99fa108
Lester Narvasta Ramirez <lesternr@gmail.com>
2022-12-02 00:10:02 -0500

    problem day 01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,34 @@
+import kotlin.math.max
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var max = 0
+        var sum = 0
+        input.forEach{ calories ->
+            if (calories == ""){
+                max = max(max, sum)
+                sum = 0
+            }else{
+                sum += calories.toInt()
+            }
+        }
+        return max(max, sum)
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val caloriesList = arrayListOf<Int>()
+        var sum = 0
+        input.forEach{ calories ->
+            if (calories == ""){
+                caloriesList.add(sum)
+                sum = 0
+            }else{
+                sum += calories.toInt()
+            }
+        }
+        caloriesList.add(sum)
+        return caloriesList.sortedDescending().take(3).sum()
     }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
     val input = readInput("Day01")
     println(part1(input))
